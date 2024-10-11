3b0019dcc7f45b078bd46b3f4fdf2a6896b694d5
Haein-Jo <95403213+Haein-Jo@users.noreply.github.com>
2024-10-11 14:19:42 +0900

    일부 명칭변경 수정 (#42)

--- src/api/groupApi.js
+++ src/api/groupApi.js
@@ -5,9 +5,9 @@ import { API_SERVER_HOST } from "./mainApi";
 const prefix = `${API_SERVER_HOST}/api/group`;
 
 // 소모임 생성하기(파일첨부)
-export const groupRegister = async (group) => {
+export const groupRegister = async (groupInfo) => {
   try {
-    const res = await axiosInstance.post(`${prefix}/register`, group);
+    const res = await axiosInstance.post(`${prefix}/register`, groupInfo);
     return res.data;
   } catch {
     return null;
@@ -15,9 +15,19 @@ export const groupRegister = async (group) => {
 };
 
 // 소모임 데이터 가져오기
-export const getGroupDetails = async (id) => {
+export const getGroupDetails = async (groupId) => {
   try {
-    const res = await axiosInstance.get(`${prefix}/${id}`);
+    const res = await axiosInstance.get(`${prefix}/detail/${groupId}`);
+    return res.data;
+  } catch {
+    return null;
+  }
+};
+
+// 로그인한 유저의 그룹가입상태 가져오기
+export const getGroupUserState = async (groupId) => {
+  try {
+    const res = await axiosInstance.get(`${prefix}/state/${groupId}`);
     return res.data;
   } catch {
     return null;
